1fc73f229592d76a17440498d7973493cbce728f
Aaron Unger <aaron.unger@gmail.com>
2014-06-11 15:24:48 -0700

    Fix handling of 'content' in model
    The 'content' value is a rich object (hash or array), but the form wants
    to deal with it as a string. Also, we need to be able to store invalid
    input from the form so we can present it back to the user to get
    corrections without reverting it to the old content.

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,4 +2,28 @@ class Document < ActiveRecord::Base
   belongs_to :owner, inverse_of: :documents, class_name: 'User'
 
   scope :shared, -> { where(shared: true) }
+
+  validate :validate_form_content
+
+  def form_content=(new_content)
+    if new_content.is_json?
+      self.content = JSON.parse(new_content)
+      @form_content = nil
+    else
+      @form_content = new_content
+    end
+  end
+
+  def form_content
+    @form_content || (content && content.to_json) || ""
+  end
+
+  protected
+
+  def validate_form_content
+    if form_content && !form_content.is_json?
+      errors.add(:form_content, "must be valid json")
+    end
+  end
+
 end
