fe92d821df5089c9adde2da87b4217ab39642605
Aaron Unger <aaron.unger@gmail.com>
2014-06-12 11:34:38 -0700

    Fix defaults for smtp settings

--- config/initializers/00_settings_defaults.rb
+++ config/initializers/00_settings_defaults.rb
@@ -10,9 +10,10 @@ Settings.defaults['admin_password'] = ENV['ADMIN_PASSWORD'] || 'password'
 Settings.defaults['admin_username'] = ENV['ADMIN_USERNAME'] || 'admin'
 
 Settings.defaults['mailer_domain_name']                = ENV['DOMAIN_NAME']                       || 'example.com'
-Settings.defaults['mailer_delivery_method']            = ENV['MAILER_DELIVERY_METHOD']            || :file
+Settings.defaults['mailer_delivery_method']            = ENV['MAILER_DELIVERY_METHOD'].nil? ? :file : ENV['MAILER_DELIVERY_METHOD'].to_sym
 Settings.defaults['mailer_smtp_user_name']             = ENV['MAILER_SMTP_USER_NAME']             || 'user'
 Settings.defaults['mailer_smtp_password']              = ENV['MAILER_SMTP_PASSWORD']              || 'password'
-Settings.defaults['mailer_smtp_authentication_method'] = ENV['MAILER_SMTP_AUTHENTICATION_METHOD'] || 'plain'
-Settings.defaults['mailer_smtp_port']                  = ENV['MAILER_SMTP_PORT'].to_i             || 587
+Settings.defaults['mailer_smtp_authentication_method'] = ENV['MAILER_SMTP_AUTHENTICATION_METHOD'].nil? ? :login : ENV['MAILER_SMTP_AUTHENTICATION_METHOD'].to_sym
+Settings.defaults['mailer_smtp_host']                  = ENV['MAILER_SMTP_HOST']                  || 'smtp.gmail.com'
+Settings.defaults['mailer_smtp_port']                  = ENV['MAILER_SMTP_PORT'].nil? ? 587 : ENV['MAILER_SMTP_PORT'].to_i
 Settings.defaults['mailer_smtp_starttls_auto']         = ENV['MAILER_SMTP_STARTTLS_AUTO'].nil? ? true : ENV['MAILER_SMTP_STARTTLS_AUTO']
--- config/initializers/actionmailer.rb
+++ config/initializers/actionmailer.rb
@@ -3,11 +3,11 @@
 ActionMailer::Base.default_url_options = { :host => Settings.mailer_domain_name }
 ActionMailer::Base.delivery_method = Settings.mailer_delivery_method
 ActionMailer::Base.smtp_settings = {
-  address: Settings.mailer_smtp_address,
+  address: Settings.mailer_smtp_host,
   port: Settings.mailer_smtp_port,
   domain: Settings.mailer_domain_name,
   authentication: Settings.mailer_smtp_authentication_method,
   enable_starttls_auto: Settings.mailer_smtp_starttls_auto,
-  user_name: Settings.mailer_smtp_username,
+  user_name: Settings.mailer_smtp_user_name,
   password: Settings.mailer_smtp_password
 }
