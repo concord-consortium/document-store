1ac25eeb76ed63ff8312bb6166f073bd1c270c19
Aaron Unger <aaron.unger@gmail.com>
2014-06-17 11:28:06 -0700

    After signing in, return the user to where they came from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     raise ActiveRecord::RecordNotFound
   end
+
+  private
+
+  def after_sign_in_path_for(resource)
+    session['user_return_to'] || root_path
+  end
 end
