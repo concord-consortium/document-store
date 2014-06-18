f3c35c4fe788255925432732aa112a2efe356576
Aaron Unger <aaron.unger@gmail.com>
2014-06-18 08:42:06 -0700

    Enable CORS support for CODAP API
    [#73227604]

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,12 @@ module Documentstore
 
     config.active_record.schema_format = :sql
 
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options], credentials: true
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
