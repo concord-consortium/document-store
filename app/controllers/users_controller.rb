f834b24674a196559ccb5976cbbf7cda7ec971ae
Aaron Unger <aaron.unger@gmail.com>
2014-06-19 13:19:50 -0700

    Report 401 instead of 404 when user info is requested but nobody is logged in
    [#72554506]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,18 +14,23 @@ class UsersController < ApplicationController
 
   # Generates CODAP-style user info, suitable for passing into its authentication routines
   def info
-    @user = current_user
-    authorize! :info, @user
-    render json: {
-      valid: true,
-      sessionToken: "abc123", # unnecessary, but here to avoid breaking anything
-      enableLogging: false,   # disabled, for now
-      privileges: 0,          # 1 means developer. For now, nobody is a developer.
-      useCookie: false,       # ???
-      enableSave: true,       # Saving is good.
-      username: @user.username,
-      name: @user.name
-    }
+    if current_user
+      @user = current_user
+      authorize! :info, @user
+      render json: {
+        valid: true,
+        sessionToken: "abc123", # unnecessary, but here to avoid breaking anything
+        enableLogging: false,   # disabled, for now
+        privileges: 0,          # 1 means developer. For now, nobody is a developer.
+        useCookie: false,       # ???
+        enableSave: true,       # Saving is good.
+        username: @user.username,
+        name: @user.name
+      }
+    else
+      authorize! :info, :nil_user
+      render json: {valid: false}, status: 401
+    end
   end
 
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,7 @@ class Ability
   def initialize(user)
     # Stuff everyone can do
     can [:read, :open], Document.shared
+    can [:info], :nil_user
 
     if user
       # Stuff logged in people can do
