3ef0f8d82389323df39d8c4e14561ea38ccbd79c
Aaron Unger <aaron.unger@gmail.com>
2014-09-30 09:08:10 -0700

    Add Launch link to document server list UI which points to the latest CODAP
    [#78742620]

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,9 +1,10 @@
 module DocumentsHelper
   def codap_link(codap_server, document)
     data = {
-      "documentServer" => root_url,
-      "runKey" => @runKey
+      "documentServer" => root_url
     }
+    rkey = (@runKey || document.run_key)
+    data["runKey"] = rkey if rkey
     if document.is_a?(Document)
       data["doc"] = document.title
       data["owner"] = document.owner.username if document.owner
@@ -11,7 +12,7 @@ module DocumentsHelper
       data["moreGames"] = document
     end
 
-    url = Addressable::URI.parse(codap_server)
+    url = Addressable::URI.parse(codap_server || ENV['DEFAULT_CODAP_URL'] || 'http://codap.concord.org/releases/latest/')
     new_query = url.query_values || {}
     new_query.merge!(data)
     url.query_values = new_query
