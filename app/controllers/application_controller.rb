977c490efac12d8ddeaae0af42d32515985fdcea
Aaron Unger <aaron.unger@gmail.com>
2014-12-09 11:39:28 -0800

    Make sure we clear the return path after logging in...
    So we don't later get sent back there again, when it's not the
    appropriate destination.
    
    [#79969984]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(resource)
-    session['user_return_to'] || root_path
+    session.delete('user_return_to') || root_path
   end
 
   def current_ability
