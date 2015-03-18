7ddf9736d5fa99be203aa89e59e3f6c6bbefb250
Aaron Unger <aaron.unger@gmail.com>
2015-03-18 14:18:47 -0700

    Allow embedding the document server in an iframe

--- config/application.rb
+++ config/application.rb
@@ -46,5 +46,8 @@ module Documentstore
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Allow embedding this app within an iframe
+    config.action_dispatch.default_headers['X-Frame-Options'] = 'ALLOWALL'
   end
 end
