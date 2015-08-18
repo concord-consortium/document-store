4e9442be9f12edeb93182acc929cf614ea3ed18b
Aaron Unger <aunger@concord.org>
2015-08-18 14:53:16 -0700

    Ensure XHR will expose the new header

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Documentstore
     config.middleware.insert_before Warden::Manager, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, expose: ['Document-Id'], methods: [:get, :post, :options], credentials: true
+        resource '*', headers: :any, expose: ['Document-Id', 'X-CODAP-Will-Overwrite'], methods: [:get, :post, :options], credentials: true
       end
     end
     # Settings in config/environments/* take precedence over those specified here.
