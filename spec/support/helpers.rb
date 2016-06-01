b43977755bf3f6417f25c777c691101a84bf96f0
Scott Cytacki <scytacki@concord.org>
2016-06-01 11:07:40 -0400

    yet more travis tweaking
    apparently the files get loaded in a different order in travis

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,3 +1,6 @@
+require_relative 'helpers/session_helpers'
+require_relative 'helpers/url_helpers'
+
 RSpec.configure do |config|
   config.include Features::SessionHelpers, type: :feature
   config.include Features::UrlHelpers, type: :feature
