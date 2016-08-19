46c6dd5c559cf34e8a113fc94807bf57545c09c3
Doug Martin <doug@zoopdoop.com>
2016-08-19 15:46:39 -0400

    Pluralized v2/document routes

--- spec/features/documents/v2_api_spec.rb
+++ spec/features/documents/v2_api_spec.rb
@@ -3,54 +3,54 @@ feature 'Document', :codap do
     describe 'open' do
       scenario 'shared documents can be opened without an access key' do
         doc = FactoryGirl.create(:document, title: 'testDoc', shared: true, content: '[1, 2, 3]')
-        visit "/v2/document/#{doc.id}"
+        visit "/v2/documents/#{doc.id}"
         expect(page).to have_content %![1,2,3]!
         expect(page.response_headers['Allow']).to eq "GET, HEAD, OPTIONS"
       end
       scenario 'shared documents can be opened by read-only access key' do
         doc = FactoryGirl.create(:document, title: 'testDoc', shared: true, content: '[1, 2, 3]', read_access_key: 'foo')
-        visit "/v2/document/#{doc.id}?accessKey=RO::foo"
+        visit "/v2/documents/#{doc.id}?accessKey=RO::foo"
         expect(page).to have_content %![1,2,3]!
         expect(page.response_headers['Allow']).to eq "GET, HEAD, OPTIONS"
       end
       scenario 'unshared documents can be opened by read-only access key' do
         doc = FactoryGirl.create(:document, title: 'testDoc', shared: false, content: '[1, 2, 3]', read_access_key: 'foo')
-        visit "/v2/document/#{doc.id}?accessKey=RO::foo"
+        visit "/v2/documents/#{doc.id}?accessKey=RO::foo"
         expect(page).to have_content %![1,2,3]!
         expect(page.response_headers['Allow']).to eq "GET, HEAD, OPTIONS"
       end
       scenario 'shared documents can be opened by read-write access key' do
         doc = FactoryGirl.create(:document, title: 'testDoc', shared: true, content: '[1, 2, 3]', read_write_access_key: 'bar')
-        visit "/v2/document/#{doc.id}?accessKey=RW::bar"
+        visit "/v2/documents/#{doc.id}?accessKey=RW::bar"
         expect(page).to have_content %![1,2,3]!
         expect(page.response_headers['Allow']).to eq "GET, HEAD, OPTIONS, PUT, PATCH"
       end
       scenario 'unshared documents can be opened by read-write access key' do
         doc = FactoryGirl.create(:document, title: 'testDoc', shared: false, content: '[1, 2, 3]', read_write_access_key: 'bar')
-        visit "/v2/document/#{doc.id}?accessKey=RW::bar"
+        visit "/v2/documents/#{doc.id}?accessKey=RW::bar"
         expect(page).to have_content %![1,2,3]!
         expect(page.response_headers['Allow']).to eq "GET, HEAD, OPTIONS, PUT, PATCH"
       end
       scenario 'documents that do not exist cannot be opened' do
-        visit "/v2/document/100000"
+        visit "/v2/documents/100000"
         expect(page.status_code).to eq(404)
         expect(page).to have_content %!{"valid":false,"message":"error.notFound"}!
       end
       scenario 'unshared documents cannot be opened without an access key' do
         doc = FactoryGirl.create(:document, title: 'testDoc', shared: false, content: '[1, 2, 3]')
-        visit "/v2/document/#{doc.id}"
+        visit "/v2/documents/#{doc.id}"
         expect(page.status_code).to eq(400)
         expect(page).to have_content %!{"valid":false,"errors":["Missing accessKey parameter"],"message":"error.missingParam"}!
       end
       scenario 'documents cannot be opened by an invalid read-only access key' do
         doc = FactoryGirl.create(:document, title: 'testDoc', shared: false, content: '[1, 2, 3]')
-        visit "/v2/document/#{doc.id}?accessKey=RO::invalid"
+        visit "/v2/documents/#{doc.id}?accessKey=RO::invalid"
         expect(page.status_code).to eq(400)
         expect(page).to have_content %!{"valid":false,"errors":["Invalid accessKey"],"message":"error.invalidAccessKey"}!
       end
       scenario 'documents cannot be opened by an invalid read-write access key' do
         doc = FactoryGirl.create(:document, title: 'testDoc', shared: false, content: '[1, 2, 3]')
-        visit "/v2/document/#{doc.id}?accessKey=RW::invalid"
+        visit "/v2/documents/#{doc.id}?accessKey=RW::invalid"
         expect(page.status_code).to eq(400)
         expect(page).to have_content %!{"valid":false,"errors":["Invalid accessKey"],"message":"error.invalidAccessKey"}!
       end
@@ -59,7 +59,7 @@ feature 'Document', :codap do
     describe 'save' do
       scenario 'documents can be saved using valid read-write access key' do
         doc = FactoryGirl.create(:document, title: "newdoc", form_content: '{ "foo": "bar" }', read_write_access_key: 'foo')
-        page.driver.browser.submit :put, "/v2/document/#{doc.id}?accessKey=RW::foo", '{ "def": [1,2,3,4] }'
+        page.driver.browser.submit :put, "/v2/documents/#{doc.id}?accessKey=RW::foo", '{ "def": [1,2,3,4] }'
         doc.reload()
         expect(doc.title).to eq("newdoc")
         expect(doc.read_write_access_key).to eq("foo")
@@ -67,28 +67,28 @@ feature 'Document', :codap do
       end
 
       scenario 'documents that do not exist cannot be saved' do
-        page.driver.browser.submit :put, "/v2/document/100000", ""
+        page.driver.browser.submit :put, "/v2/documents/100000", ""
         expect(page.status_code).to eq(404)
         expect(page).to have_content %!{"valid":false,"message":"error.notFound"}!
       end
 
       scenario 'documents cannot be saved using invalid read-write access key' do
         doc = FactoryGirl.create(:document, title: 'testDoc', shared: false, content: '[1, 2, 3]')
-        page.driver.browser.submit :put, "/v2/document/#{doc.id}?accessKey=RW::invalid", '{ "def": [1,2,3,4] }'
+        page.driver.browser.submit :put, "/v2/documents/#{doc.id}?accessKey=RW::invalid", '{ "def": [1,2,3,4] }'
         expect(page.status_code).to eq(400)
         expect(page).to have_content %!{"valid":false,"errors":["Invalid accessKey"],"message":"error.invalidAccessKey"}!
       end
 
       scenario 'documents cannot be saved with a missing read-write access key' do
         doc = FactoryGirl.create(:document, title: 'testDoc', shared: false, content: '[1, 2, 3]')
-        page.driver.browser.submit :put, "/v2/document/#{doc.id}", '{ "def": [1,2,3,4] }'
+        page.driver.browser.submit :put, "/v2/documents/#{doc.id}", '{ "def": [1,2,3,4] }'
         expect(page.status_code).to eq(400)
         expect(page).to have_content %!{"valid":false,"errors":["Missing accessKey parameter"],"message":"error.missingParam"}!
       end
 
       scenario 'documents cannot be saved using read-only access key' do
         doc = FactoryGirl.create(:document, title: 'testDoc', shared: false, content: '[1, 2, 3]')
-        page.driver.browser.submit :put, "/v2/document/#{doc.id}?accessKey=RO::foo", '{ "def": [1,2,3,4] }'
+        page.driver.browser.submit :put, "/v2/documents/#{doc.id}?accessKey=RO::foo", '{ "def": [1,2,3,4] }'
         expect(page.status_code).to eq(400)
         expect(page).to have_content %!{"valid":false,"errors":["Invalid accessKey type"],"message":"error.invalidAccessKeyType"}!
       end
@@ -98,7 +98,7 @@ feature 'Document', :codap do
       scenario 'documents can be patched' do
         # take from sample at http://jsonpatch.com/
         doc = FactoryGirl.create(:document, title: "newdoc", content: '{"foo":"bar","baz":"qux"}', read_write_access_key: 'foo')
-        page.driver.browser.submit :patch, "/v2/document/#{doc.id}?accessKey=RW::foo", '[{ "op": "replace", "path": "/baz", "value": "boo" },  { "op": "add", "path": "/hello", "value": ["world"] },  { "op": "remove", "path": "/foo"}]'
+        page.driver.browser.submit :patch, "/v2/documents/#{doc.id}?accessKey=RW::foo", '[{ "op": "replace", "path": "/baz", "value": "boo" },  { "op": "add", "path": "/hello", "value": ["world"] },  { "op": "remove", "path": "/foo"}]'
         expect(page.status_code).to eq(200)
         expect(page).to have_content %!{"status":"Patched","valid":true,"id":#{doc.id}}!
         doc.reload()
@@ -106,49 +106,49 @@ feature 'Document', :codap do
       end
 
       scenario 'documents that do not exist cannot be patched' do
-        page.driver.browser.submit :patch, "/v2/document/100000", ""
+        page.driver.browser.submit :patch, "/v2/documents/100000", ""
         expect(page.status_code).to eq(404)
         expect(page).to have_content %!{"valid":false,"message":"error.notFound"}!
       end
 
       scenario 'documents cannot be patched using empty patch set' do
         doc = FactoryGirl.create(:document, title: "newdoc", content: '{"foo":"bar","baz":"qux"}', read_write_access_key: 'foo')
-        page.driver.browser.submit :patch, "/v2/document/#{doc.id}?accessKey=RW::foo", ''
+        page.driver.browser.submit :patch, "/v2/documents/#{doc.id}?accessKey=RW::foo", ''
         expect(page.status_code).to eq(400)
         expect(page).to have_content %!{"status":"Error","errors":["Invalid patch JSON (parsing)",!
       end
 
       scenario 'documents cannot be patched using invalid json for patch set' do
         doc = FactoryGirl.create(:document, title: "newdoc", content: '{"foo":"bar","baz":"qux"}', read_write_access_key: 'foo')
-        page.driver.browser.submit :patch, "/v2/document/#{doc.id}?accessKey=RW::foo", '{"bam"'
+        page.driver.browser.submit :patch, "/v2/documents/#{doc.id}?accessKey=RW::foo", '{"bam"'
         expect(page.status_code).to eq(400)
         expect(page).to have_content %!{"status":"Error","errors":["Invalid patch JSON (parsing)",!
       end
 
       scenario 'documents cannot be patched using valid json but invalid patch set' do
         doc = FactoryGirl.create(:document, title: "newdoc", content: '{"foo":"bar","baz":"qux"}', read_write_access_key: 'foo')
-        page.driver.browser.submit :patch, "/v2/document/#{doc.id}?accessKey=RW::foo", '{"bing":"boom"}'
+        page.driver.browser.submit :patch, "/v2/documents/#{doc.id}?accessKey=RW::foo", '{"bing":"boom"}'
         expect(page.status_code).to eq(400)
         expect(page).to have_content %!{"status":"Error","errors":["Invalid patch JSON (parsing)",!
       end
 
       scenario 'documents cannot be patched using invalid read-write access key' do
         doc = FactoryGirl.create(:document, title: 'testDoc', shared: false, content: '[1, 2, 3]')
-        page.driver.browser.submit :patch, "/v2/document/#{doc.id}?accessKey=RW::invalid", '{ "def": [1,2,3,4] }'
+        page.driver.browser.submit :patch, "/v2/documents/#{doc.id}?accessKey=RW::invalid", '{ "def": [1,2,3,4] }'
         expect(page.status_code).to eq(400)
         expect(page).to have_content %!{"valid":false,"errors":["Invalid accessKey"],"message":"error.invalidAccessKey"}!
       end
 
       scenario 'documents cannot be patched with a missing read-write access key' do
         doc = FactoryGirl.create(:document, title: 'testDoc', shared: false, content: '[1, 2, 3]')
-        page.driver.browser.submit :patch, "/v2/document/#{doc.id}", '{ "def": [1,2,3,4] }'
+        page.driver.browser.submit :patch, "/v2/documents/#{doc.id}", '{ "def": [1,2,3,4] }'
         expect(page.status_code).to eq(400)
         expect(page).to have_content %!{"valid":false,"errors":["Missing accessKey parameter"],"message":"error.missingParam"}!
       end
 
       scenario 'documents cannot be patched using read-only access key' do
         doc = FactoryGirl.create(:document, title: 'testDoc', shared: false, content: '[1, 2, 3]')
-        page.driver.browser.submit :patch, "/v2/document/#{doc.id}?accessKey=RO::foo", '{ "def": [1,2,3,4] }'
+        page.driver.browser.submit :patch, "/v2/documents/#{doc.id}?accessKey=RO::foo", '{ "def": [1,2,3,4] }'
         expect(page.status_code).to eq(400)
         expect(page).to have_content %!{"valid":false,"errors":["Invalid accessKey type"],"message":"error.invalidAccessKeyType"}!
       end
@@ -157,7 +157,7 @@ feature 'Document', :codap do
     describe 'copy_shared' do
       scenario 'shared documents can be copied using source id' do
         doc = FactoryGirl.create(:document, title: 'testDoc', shared: true, content: '[1, 2, 3]')
-        page.driver.browser.submit :post, "/v2/document?source=#{doc.id}", ""
+        page.driver.browser.submit :post, "/v2/documents?source=#{doc.id}", ""
         expect(page.status_code).to eq(200)
         expect(page).to have_content %!{"status":"Copied","valid":true,!
         response = JSON.parse(page.body)
@@ -173,28 +173,28 @@ feature 'Document', :codap do
 
       scenario 'unshared documents cannot be copied using source id' do
         doc = FactoryGirl.create(:document, title: 'testDoc', shared: false, content: '[1, 2, 3]')
-        page.driver.browser.submit :post, "/v2/document?source=#{doc.id}", ""
+        page.driver.browser.submit :post, "/v2/documents?source=#{doc.id}", ""
         expect(page.status_code).to eq(403)
         expect(page).to have_content %!{"valid":false,"errors":["Source document is not shared"],"message":"error.notShared"}!
       end
 
       scenario 'documents cannot be copied with a missing source id' do
         doc = FactoryGirl.create(:document, title: 'testDoc', shared: false, content: '[1, 2, 3]')
-        page.driver.browser.submit :post, "/v2/document", ""
+        page.driver.browser.submit :post, "/v2/documents", ""
         expect(page.status_code).to eq(400)
         expect(page).to have_content %!{"valid":false,"errors":["Missing source parameter"],"message":"error.missingParam"}!
       end
 
       scenario 'shared documents cannot be copied using read-only access key' do
         doc = FactoryGirl.create(:document, title: 'testDoc', shared: true, content: '[1, 2, 3]', read_access_key: 'foo')
-        page.driver.browser.submit :post, "/v2/document?sourceAccessKey=RO::foo", ""
+        page.driver.browser.submit :post, "/v2/documents?sourceAccessKey=RO::foo", ""
         expect(page.status_code).to eq(400)
         expect(page).to have_content %!{"valid":false,"errors":["Missing source parameter"],"message":"error.missingParam"}!
       end
 
       scenario 'shared documents cannot be copied using read-write access key' do
         doc = FactoryGirl.create(:document, title: 'testDoc', shared: true, content: '[1, 2, 3]', read_write_access_key: 'foo')
-        page.driver.browser.submit :post, "/v2/document?sourceAccessKey=RW::foo", ""
+        page.driver.browser.submit :post, "/v2/documents?sourceAccessKey=RW::foo", ""
         expect(page.status_code).to eq(400)
         expect(page).to have_content %!{"valid":false,"errors":["Missing source parameter"],"message":"error.missingParam"}!
       end
