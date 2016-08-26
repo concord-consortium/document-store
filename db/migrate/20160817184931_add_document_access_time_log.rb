9de008fc3bf148f1cbf533834b812b7a8c5bd0af
Doug Martin <doug@zoopdoop.com>
2016-08-26 10:44:41 -0400

    Fixed last two migrations
    - changed access key migration to only run on up and skip validations
    - added missing change method wrapper in access time log migration

--- db/migrate/20160817184931_add_document_access_time_log.rb
+++ db/migrate/20160817184931_add_document_access_time_log.rb
@@ -1,9 +1,11 @@
 class AddDocumentAccessTimeLog < ActiveRecord::Migration
-  create_table :document_access_log do |t|
-    t.integer :document_id
-    t.string :api_version
-    t.string :action
-    t.string :access_params
-    t.timestamp :accessed_at
+  def change
+    create_table :document_access_log do |t|
+      t.integer :document_id
+      t.string :api_version
+      t.string :action
+      t.string :access_params
+      t.timestamp :accessed_at
+    end
   end
 end
