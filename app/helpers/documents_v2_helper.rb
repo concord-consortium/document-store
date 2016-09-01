d1a4307284229021249aedfb458d317238435ccb
Doug Martin <doug@zoopdoop.com>
2016-09-01 09:05:38 -0400

    Removed fromLaunchPage param to codap_v2_link helper

--- app/helpers/documents_v2_helper.rb
+++ app/helpers/documents_v2_helper.rb
@@ -1,9 +1,8 @@
 module DocumentsV2Helper
-  def codap_v2_link(codap_server, fromLaunchPage=false)
+  def codap_v2_link(codap_server)
     data = {
       "documentServer" => root_url(protocol: 'https')
     }
-    data["launchFromLara"] = 'true' if fromLaunchPage
 
     url = Addressable::URI.parse(codap_server || ENV['CODAP_DEFAULT_URL'] || 'https://codap.concord.org/releases/latest/')
     new_query = url.query_values || {}
